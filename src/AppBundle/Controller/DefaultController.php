0056143c762a3763775e797ac8b87d09f2e6dfea
Fyodorov Alexei <alexei.fyodorov.phpdevelopment@gmail.com>
2015-10-16 17:35:05 +0200

    Use Bootstrapp template

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Acme\PostBundle\Entity\Post;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,9 +14,40 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        $repository = $this->getDoctrine()
+            ->getRepository('AcmePostBundle:Post');
+
+        // find *all* products
+        $posts = $repository->findAll();
+
+        $s_header_title = "Welcome To Our Studio!";
+        $s_header_description = "It's Nice To Meet You";
+
+        /*
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
+        */
+
+        return $this->render(
+            // 'post/recent_list.html.twig',
+            'default/index.html.twig',
+            array(
+                'posts' => $posts
+            )
+        );
+
+    }
+
+    public function recentPostsAction($max = 3)
+    {
+        // make a database call or other logic
+        // to get the "$max" most recent articles
+        $posts = array();
+
+        return $this->render(
+            'post/recent_list.html.twig',
+            array('posts' => $posts)
+        );
     }
 }
